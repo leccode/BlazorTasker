095869d3440d069b5812d338a82cf6c290901dce
leccode <costas.lekkos@gmail.com>
2026-02-19 19:55:21 +0200

    feat(ui): add reverse string algorithm

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-using BlazorTasker;
+using BlazorTasker.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<App>("#app"); // index.html
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
